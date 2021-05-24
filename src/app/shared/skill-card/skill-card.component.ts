c40130cc6e4b869e87d7e7b06e55a8ae6cf5bab2
CDiala <dialachibuzo@yahoo.com>
2021-05-24 14:00:20 +0100

    Designed and implemented the reusable skill card component.

--- src/app/shared/skill-card/skill-card.component.ts
+++ src/app/shared/skill-card/skill-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skill-card',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillCardComponent implements OnInit {
 
+  @Input() content!: string;
+  @Input() image!: string;
+
+  count!: number;
+  extract!: string;
+
+  // skill = '5 completed projects';
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void  {
+    this.getCount();
   }
 
+  getCount() {
+    this.count = +this.content.split(' ')[0];
+    this.extract = this.content.substring(this.content.indexOf(' '));
+    console.log('count:', this.count, 'extract:', this.extract);
+    // console.log(this.content.split(' '));
+    // console.log(this.content.split(' ').findIndex(char => char === ' '));
+  }
 }

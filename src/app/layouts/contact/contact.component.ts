f10a8b00110f343bf3bb436f1f9e5fc83e091236
CDiala <dialachibuzo@yahoo.com>
2021-06-07 13:15:05 +0100

    Created and implemented a reactive form for the contact section

--- src/app/layouts/contact/contact.component.ts
+++ src/app/layouts/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faComment, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+// import { setTimeout } from 'timers';
 
 @Component({
   selector: 'app-contact',
@@ -11,9 +12,38 @@ export class ContactComponent implements OnInit {
 
   constructor() { }
   faEnvelop = faEnvelope;
-  faUser = faUser
-  faComment = faComment
+  faUser = faUser;
+  faComment = faComment;
+  success?: boolean;
+
+  mailForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    comment: new FormControl('', Validators.required)
+  });
+
+  get name() {
+    return this.mailForm.get('name');
+  }
+
+  get email() {
+    return this.mailForm.get('email');
+  }
+
+  get comment() {
+    return this.mailForm.get('comment');
+  }
+
   ngOnInit(): void {
   }
 
+  onSubmit($event: any) {
+    console.log('hello', this.mailForm.value);
+    this.success = true;
+
+    setTimeout(() => {
+      this.mailForm.reset();
+      this.success = false;
+    }, 5000) 
+  }
 }
